2d32c471c84698c12ccb4286f3fcaad9c7eab9f4
joseg-alvesg <cinhoalves85@gmail.com>
2024-04-18 12:49:56 -0300

    fix category render

--- src/components/CategoryBar/index.jsx
+++ src/components/CategoryBar/index.jsx
@@ -1,46 +1,55 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import SearchContext from '../../context/SearchContext';
-import { fetchCategories } from '../../util/FetchFunctions';
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import SearchContext from "../../context/SearchContext";
+import { fetchCategories } from "../../util/FetchFunctions";
 
 const MAX_CATEGORIES = 5;
 
 export default function CategoryBar() {
   const { dbData, searchRecipes, searchByCategory } = useContext(SearchContext);
   const [categories, setCategories] = useState([]);
-  const [selectedCatedory, setSelectedCategory] = useState('');
+  const [selectedCatedory, setSelectedCategory] = useState("");
   const history = useHistory();
 
   const categoriesLoad = useCallback(async () => {
-    const db = history.location.pathname === '/meals' ? 'themealdb' : 'thecocktaildb';
+    const db =
+      history.location.pathname === "/meals" ? "themealdb" : "thecocktaildb";
     const res = await fetchCategories(db);
-    console.log(res);
     setCategories(res.meals || res.drinks);
   }, [history.location.pathname]);
 
   useEffect(() => {
     categoriesLoad();
-  }, [dbData, categoriesLoad]);
+  }, [categoriesLoad]);
 
-  const handleSearch = async (category) => {
-    const route = history.location.pathname;
-    if (selectedCatedory === category) {
-      searchRecipes(route);
-      return;
-    }
-    await searchByCategory(route, undefined, category);
-    setSelectedCategory(category);
-  };
+  const handleSearch = useCallback(
+    async (category) => {
+      const route = history.location.pathname;
+      if (selectedCatedory === category) {
+        setSelectedCategory("");
+        searchRecipes(route);
+        return;
+      }
+      await searchByCategory(route, undefined, category);
+      setSelectedCategory(category);
+    },
+    [
+      history.location.pathname,
+      searchByCategory,
+      searchRecipes,
+      selectedCatedory,
+    ],
+  );
 
   return (
     <div>
-      {categories
-        && categories.slice(0, MAX_CATEGORIES).map((category) => (
+      {categories &&
+        categories.slice(0, MAX_CATEGORIES).map((category) => (
           <button
-            key={ category.strCategory }
-            data-testid={ `${category.strCategory}-category-filter` }
+            key={category.strCategory}
+            data-testid={`${category.strCategory}-category-filter`}
             type="button"
-            onClick={ () => handleSearch(category.strCategory) }
+            onClick={() => handleSearch(category.strCategory)}
           >
             {category.strCategory}
           </button>
@@ -48,7 +57,7 @@ export default function CategoryBar() {
       <button
         data-testid="All-category-filter"
         type="button"
-        onClick={ () => searchRecipes(history.location.pathname) }
+        onClick={() => searchRecipes(history.location.pathname)}
       >
         All
       </button>
--- src/context/SearchProvider.jsx
+++ src/context/SearchProvider.jsx
@@ -1,30 +1,30 @@
-import PropTypes from 'prop-types';
-import { useCallback, useState, useMemo } from 'react';
-import SearchContext from './SearchContext';
+import PropTypes from "prop-types";
+import { useCallback, useState, useMemo } from "react";
+import SearchContext from "./SearchContext";
 import {
   fetchCategories,
   fetchDetails,
   fetchRecipes,
-} from '../util/FetchFunctions';
+} from "../util/FetchFunctions";
 
 export default function SearchProvider({ children }) {
   const [searchBar, setSearchBar] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [filterType, setFilterType] = useState();
   const [dbData, setDbData] = useState([]);
   const [recipeDetails, setRecipeDetails] = useState({});
   const [recommendations, setRecommendations] = useState([]);
 
   const searchRecipes = useCallback(
-    async (route, filter = 'name', content = '') => {
-      const db = route === '/meals' ? 'themealdb' : 'thecocktaildb';
+    async (route, filter = "name", content = "") => {
+      const db = route === "/meals" ? "themealdb" : "thecocktaildb";
       const res = await fetchRecipes(db, filter, content);
       if (!res.meals && !res.drinks) {
-        global.alert('Sorry, we haven\'t found any recipes for these filters.');
+        global.alert("Sorry, we haven't found any recipes for these filters.");
         return;
       }
       const data = res.meals || res.drinks;
-      console.log('provider data', data);
+      // console.log("provider data", data);
       setDbData(data);
       return data;
     },
@@ -32,32 +32,34 @@ export default function SearchProvider({ children }) {
   );
 
   const searchByCategory = useCallback(
-    async (route, filter = 'filter', category = '') => {
-      const db = route === '/meals' ? 'themealdb' : 'thecocktaildb';
+    async (route, filter = "filter", category = "") => {
+      const db = route === "/meals" ? "themealdb" : "thecocktaildb";
       const res = await fetchCategories(db, filter, category);
       const data = res.meals || res.drinks;
+      setDbData(data);
+      // console.log("searchByCategory", data);
       return data;
     },
     [],
   );
 
   const searchById = useCallback(async (route, id) => {
-    const db = route === 'meals' ? 'themealdb' : 'thecocktaildb';
+    const db = route === "meals" ? "themealdb" : "thecocktaildb";
     const res = await fetchDetails(db, id);
     const data = res.meals || res.drinks;
     if (data[0].strYoutube) {
-      data[0].strYoutube = data[0].strYoutube.replace('watch?v=', 'embed/');
+      data[0].strYoutube = data[0].strYoutube.replace("watch?v=", "embed/");
     } else if (data[0].strVideo) {
-      data[0].srtVideo = data[0].strVideo.replace('watch?v=', 'embed/');
+      data[0].srtVideo = data[0].strVideo.replace("watch?v=", "embed/");
     }
-    console.log('data[0]', data[0]);
+    console.log("data[0]", data[0]);
     setRecipeDetails(data[0]);
     return data[0];
   }, []);
 
   const getRecommends = useCallback(async (route) => {
-    const db = route === 'meals' ? 'themealdb' : 'thecocktaildb';
-    const res = await fetchRecipes(db, 'name', '');
+    const db = route === "meals" ? "themealdb" : "thecocktaildb";
+    const res = await fetchRecipes(db, "name", "");
     const data = res.meals || res.drinks;
     setRecommendations(data);
     return data;
@@ -101,7 +103,7 @@ export default function SearchProvider({ children }) {
   );
 
   return (
-    <SearchContext.Provider value={ value }>{children}</SearchContext.Provider>
+    <SearchContext.Provider value={value}>{children}</SearchContext.Provider>
   );
 }
 
