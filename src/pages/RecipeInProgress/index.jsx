5e98b05a7f4ba77d225d6e3a028255260102b826
joseg-alvesg <cinhoalves85@gmail.com>
2024-04-19 23:00:19 -0300

    progress progress

--- src/pages/RecipeInProgress/index.jsx
+++ src/pages/RecipeInProgress/index.jsx
@@ -1,34 +1,66 @@
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import {
   useHistory,
   useParams,
-} from 'react-router-dom/cjs/react-router-dom.min';
-import SearchContext from '../../context/SearchContext';
+} from "react-router-dom/cjs/react-router-dom.min";
+import useRecipeDetails from "../../helpers/hooks/useRecipeDetails";
+import {
+  getInProgressRecipes,
+  saveInProgressRecipes,
+} from "../../util/localStorageHelper";
 
 export default function RecipeInProgress() {
-  const { recipeDetails, ingredients, searchById } = useContext(SearchContext);
-  const history = useHistory();
+  const [recipeDetails, ingredients] = useRecipeDetails();
+  const [checkedIngredients, setCheckedIngredients] = useState([]);
   const { id } = useParams();
-
-  const handleSearch = useCallback(async () => {
-    const route = history.location.pathname.split('/')[1];
-    await searchById(route, id);
-    handleIngredients();
-  }, [history.location.pathname, searchById]);
+  const history = useHistory();
 
   useEffect(() => {
-    handleSearch();
-  }, [handleSearch]);
+    const route = history.location.pathname.split("/")[1];
+    if (
+      getInProgressRecipes() &&
+      getInProgressRecipes()[route] &&
+      getInProgressRecipes()[route][id]
+    ) {
+      setCheckedIngredients(getInProgressRecipes()[route][id]);
+      return;
+    } else {
+      console.log("aqui");
+      const hasIngredients = ingredients.reduce((acc, ingredient) => {
+        acc[ingredient] = false;
+        return acc;
+      }, {});
+      setCheckedIngredients(hasIngredients);
+    }
+  }, []);
+
+  const toggleIngredient = useCallback((value) => {
+    setCheckedIngredients((prevCheckedIngredients) => {
+      const newCheckedIngredients = { ...prevCheckedIngredients };
+
+      newCheckedIngredients[value] = !newCheckedIngredients[value];
+      return newCheckedIngredients;
+    });
+  }, []);
+
+  const saveIngredients = () => {
+    const route = history.location.pathname.split("/")[1];
+    const newCheckedIngredients = { ...checkedIngredients };
+    console.log("newCheckedIngredients", newCheckedIngredients);
+    saveInProgressRecipes(route, id, newCheckedIngredients);
+  };
 
   return (
     <div className=" w-100 h-100">
       <img
         data-testid="recipe-photo"
-        src={ recipeDetails.strMealThumb }
+        src={recipeDetails.strMealThumb || recipeDetails.strDrinkThumb}
         alt="recipe"
-        style={ { width: '100px' } }
+        style={{ width: "100px" }}
       />
-      <h1 data-testid="recipe-title">{recipeDetails.strMeal}</h1>
+      <h1 data-testid="recipe-title">
+        {recipeDetails.strMeal || recipeDetails.strDrink}
+      </h1>
 
       <button data-testid="share-btn">Compartilhar</button>
       <button data-testid="favorite-btn">Favoritar</button>
@@ -36,21 +68,36 @@ export default function RecipeInProgress() {
       <p data-testid="recipe-category">{recipeDetails.strCategory}</p>
 
       <p data-testid="instructions">{recipeDetails.strInstructions}</p>
-      <button data-testid="finish-recipe-btn">Finalizar Receita</button>
+
       {ingredients.map((ingredient, index) => (
-        <>
-          <label key={ index } htmlFor={ ingredient }>
-            {' '}
+        <div key={index}>
+          <label
+            key={index}
+            data-testid={`${index}-ingredient-step`}
+            style={{
+              textDecoration: checkedIngredients[ingredient]
+                ? "line-through solid rgb(0, 0, 0)"
+                : "none",
+            }}
+            onClick={(e) => toggleIngredient(e.target.id)}
+            id={ingredient}
+          >
+            <input
+              type="checkbox"
+              name={ingredient}
+              onChange={(e) => {
+                toggleIngredient(e.target.id);
+                saveIngredients(e.target.id);
+              }}
+              id={ingredient}
+              checked={checkedIngredients[ingredient] || false}
+            />
             {ingredient}
-            {' '}
           </label>
-          <input
-            key={ index }
-            type="checkbox"
-            data-testid={ `${index}-ingredient-step` }
-          />
-        </>
+        </div>
       ))}
+
+      <button data-testid="finish-recipe-btn">Finalizar Receita</button>
     </div>
   );
 }
--- src/util/localStorageHelper.js
+++ src/util/localStorageHelper.js
@@ -1,11 +1,11 @@
 export const setUser = (key, value) => {
   const valueToStore = JSON.stringify({ [key]: value });
-  localStorage.setItem('user', valueToStore);
+  localStorage.setItem("user", valueToStore);
 };
 
 export const getDoneRecipes = () => {
-  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-  console.log('getDoneRecipes -> doneRecipes', doneRecipes);
+  const doneRecipes = JSON.parse(localStorage.getItem("doneRecipes"));
+  console.log("getDoneRecipes -> doneRecipes", doneRecipes);
   return doneRecipes;
 };
 
@@ -13,8 +13,44 @@ export const saveDoneRecipes = (key, value) => {
   if (getDoneRecipes()) {
     const doneRecipes = getDoneRecipes();
     doneRecipes.push({ [key]: value });
-    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+    localStorage.setItem("doneRecipes", JSON.stringify(doneRecipes));
   } else {
-    localStorage.setItem('doneRecipes', JSON.stringify([{ [key]: value }]));
+    localStorage.setItem("doneRecipes", JSON.stringify([{ [key]: value }]));
+  }
+};
+// {
+//     drinks: {
+//         id-da-bebida: [lista-de-ingredientes-utilizados],
+//         ...
+//     },
+//     meals: {
+//         id-da-comida: [lista-de-ingredientes-utilizados],
+//         ...
+//     }
+// }
+
+export const getInProgressRecipes = () => {
+  const inProgressRecipes = JSON.parse(
+    localStorage.getItem("inProgressRecipes"),
+  );
+  return inProgressRecipes;
+};
+
+export const saveInProgressRecipes = (route, key = "123", value = "123") => {
+  if (getInProgressRecipes()) {
+    let inProgressRecipes = getInProgressRecipes();
+    inProgressRecipes = {
+      ...inProgressRecipes,
+      [route]: { ...inProgressRecipes[route], [key]: value },
+    };
+    localStorage.setItem(
+      "inProgressRecipes",
+      JSON.stringify(inProgressRecipes),
+    );
+  } else {
+    localStorage.setItem(
+      "inProgressRecipes",
+      JSON.stringify({ [route]: { [key]: value } }),
+    );
   }
 };
